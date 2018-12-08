04dbae821912739945c9d1b5924ed3ee5c08326c
catkins <catkinsjr28@gmail.com>
2018-12-08 14:56:05 -0500

    left menu html & css 2

---
+++ public/js/components/firstComp.js
@@ -0,0 +1,21 @@
+webpackJsonp([0],{
+
+/***/ 228:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _react = __webpack_require__(97);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(98);
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/***/ })
+
+},[228]);
---
+++ resources/assets/js/components/firstComp.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// class Layout extends Component {
+// 	constructor() {
+// 		super();
+// 		this.state = {
+// 			name: 'Joe'
+// 		};
+// 	}
+// 	clickedBtn = () => {};
+// 	async test() {}
+// 	render() {
+// 		return (
+// 			<div className="home">
+// 				This is my first React Component
+// 			</div>
+// 		);
+// 	}
+// }
+//
+// const app = document.getElementById('app');
+//
+// ReactDOM.render(<Layout />, app);
